f793b2745a0e2753c00bff9ea450d92b2fc8d817
Parker Rudd <parkerbrudd@gmail.com>
2022-04-08 14:18:53 -0600

    added playerCount feature

--- src/App.js
+++ src/App.js
@@ -19,43 +19,72 @@ class App extends Component {
       loading: false,
       dealCards: false,
       hit: 0,
-      stay: false
+      stay: false,
+      playerCount: 0, 
+      dealerCount: 0
     })
 
   }
 
 
   render() {
-    const {card, loading, dealCards, hit, stay} = this.state;
+    const {card, loading, dealCards, hit, stay, dealerCount, playerCount} = this.state;
 
     const deal = () => {
+      let cardOne, cardTwo
+      if (this.state.card.cards[0].value ==  "KING" || this.state.card.cards[0].value ==  "QUEEN" || this.state.card.cards[0].value ==  "JACK") {
+        cardOne = 10
+      } else if (this.state.card.cards[0].value !=  "KING" || this.state.card.cards[0].value !=  "QUEEN" || this.state.card.cards[0].value !=  "JACK") {
+        cardOne = this.state.card.cards[0].value
+      } 
+      if (this.state.card.cards[1].value ==  "KING" || this.state.card.cards[1].value ==  "QUEEN" || this.state.card.cards[1].value ==  "JACK") {
+        cardTwo = 10
+      } else if (this.state.card.cards[1].value !=  "KING" || this.state.card.cards[1].value !=  "QUEEN" || this.state.card.cards[1].value !=  "JACK") {
+        cardTwo = this.state.card.cards[1].value
+      } 
       this.setState({
-        dealCards:true
+        dealCards: true, 
+        playerCount: parseInt(cardOne) + parseInt(cardTwo)
       })
-    }; 
+      
+  }; 
 
     const hitMe = () => {
       this.setState({
         hit: this.state.hit + 1, 
       })
     }
+
+    const stayHere = () => {
+      this.setState({
+        stay: true
+      })
+    }
+
+    // const compareCount = () => {
+      
+    // }
     return (
 
     <div className='App'>
         <div className='heading'>
+          <div className="counts">
+            <p>Player Count: {this.state.playerCount}</p>
+            <p>Dealer Count: {this.state.dealerCount}</p>
+          </div>
           <h1>Blackjack</h1>
           <button onClick={deal} id="header-btn">Draw Cards</button>
         </div>
 
         <div>
-          {dealCards ? <UserDraw cards={this.state.card} anotherCard={this.state.hit}/> : ''}
-          {dealCards ? <DealerDraw cards={this.state.card}/> : ''}
+          {dealCards ? <UserDraw cards={this.state.card} anotherCard={this.state.hit} doneBetting={this.state.stay} playerCount={this.state.playerCount}/> : ''}
+          {dealCards ? <DealerDraw cards={this.state.card} reveal={this.state.stay} dealerCount={this.state.dealerCount}/> : ''}
         </div>
             {
             dealCards ?  
             <div className='player-btns'> 
                   <button onClick={hitMe} className='btns' id='hit-btn'>Hit Me</button>
-                  <button className='btns' id='stay-btn'>Stay</button>
+                  <button onClick={stayHere} className='btns' id='stay-btn'>Stay</button>
             </div> 
             : ''
             }
--- src/dealerDraw.js
+++ src/dealerDraw.js
@@ -8,8 +8,11 @@ function DealerDraw(props) {
                 <img src={props.cards.cards[2].image} alt="" />
             </div>
             <div id="dealer-card-two">
-                {/*<img src={props.cards.cards[3].image} alt="" />*/}
+                {
+                props.reveal ? <img src={props.cards.cards[3].image} alt="" />:    
                 <img src="https://i.pinimg.com/originals/10/80/a4/1080a4bd1a33cec92019fab5efb3995d.png" alt="" />
+                }
+                
             </div>
         </div>
     )
